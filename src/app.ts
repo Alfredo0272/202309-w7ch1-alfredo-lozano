336603e1656cbd2be60b688a0da4a5b5fdbdefd2
alfredo <alfloz01@ucm.es>
2023-11-20 17:47:55 +0100

    Config sonar

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import { beerRouter } from './router/beer.router.js';
+import createDebug from 'debug';
+const debug = createDebug('W7E:app');
 
 export const app = express();
+debug('Starting');
 
 app.use(cors());
 app.use(morgan('dev'));
